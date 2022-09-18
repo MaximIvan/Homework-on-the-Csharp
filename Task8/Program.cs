a24a76c3a40d23663708203525264b10fa5e8c4a
Maksim <homeice@mail.ru>
2022-09-18 05:15:57 +0300

    задача 8

---
+++ Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу, которая на вход принимает число (N), 
+// а на выходе показывает все четные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int currentNum = 1;
+
+while(currentNum <= num) 
+{
+    if(currentNum % 2 == 0) Console.WriteLine("Четное число: " + currentNum);
+    currentNum += 1;
+}
+if (num <= 1) Console.WriteLine("Четное число отсутствует");
