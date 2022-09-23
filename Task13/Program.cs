90708aba75a49b6bffd2e8d9d18899d754358385
Maksim <homeice@mail.ru>
2022-09-23 15:08:42 +0300

    Задача 13 выполнено решение для любого числа

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -4,30 +4,34 @@
 // 78 - третьей цифры нет
 // 32679 - 6
 
-int num = new Random().Next(-99999, 99999);
+int num = new Random().Next();
 
 if ((num < 100) & (num > -100)) Console.Write($"{num} - третьей цифры нет");
-
-if (((num < 1000) & (num > 100)) | ((num > -1000) & (num < -100)))
-{
-    int thirdNum = num % 10;
-    if ((thirdNum > -10) & (thirdNum < - 1))
-    thirdNum *= -1;
-    Console.Write($"{num} - {thirdNum}");
-} 
-if (((num < 10000) & (num > 1000)) | ((num > -10000) & (num < -1000)))
-{
-    int numOst = num % 100;
-    int thirdNum2 = numOst / 10;
-    if ((thirdNum2 > -10) & (thirdNum2 < - 1))
-    thirdNum2 *= -1;
-    Console.Write($"{num} - {thirdNum2}");
-} 
-if (((num < 100000) & (num > 10000)) | ((num > -100000) & (num < -10000)))
-{
-    int numOst2 = num % 1000;
-    int thirdNum3 = numOst2 / 100;
-    if ((thirdNum3 > -10) & (thirdNum3 < - 1))
-    thirdNum3 *= -1;
-    Console.Write($"{num} - {thirdNum3}");
-}
+int currentNum = num;
+while ((currentNum >= 1000) | (currentNum <= -1000))   // if (((num < 1000) & (num > 100)) | ((num > -1000) & (num < -100)))
+{                                                      //{
+    currentNum /= 10;                                  //   int thirdNum = num % 10;       
+}                                                      //   if ((thirdNum > -10) & (thirdNum < - 1))
+int thirdNum = currentNum % 10;                        //   thirdNum *= -1;
+if ((thirdNum > -10) & (thirdNum < - 1))               //   Console.Write($"{num} - {thirdNum}");
+thirdNum *= -1;                                        //} 
+Console.Write($"{num} - {thirdNum}");                  //if (((num < 10000) & (num > 1000)) | ((num > -10000) & (num < -1000)))
+                                                       //{
+                                                       //    int numOst = num % 100;
+                                                       //    int thirdNum2 = numOst / 10;
+                                                       //    if ((thirdNum2 > -10) & (thirdNum2 < - 1))
+                                                       //    thirdNum2 *= -1;
+                                                       //    Console.Write($"{num} - {thirdNum2}");
+                                                       //} 
+                                                       //if (((num < 100000) & (num > 10000)) | ((num > -100000) & (num < -10000)))
+                                                       //{
+                                                       //    int numOst2 = num % 1000;
+                                                       //    int thirdNum3 = numOst2 / 100;
+                                                       //    if ((thirdNum3 > -10) & (thirdNum3 < - 1))
+                                                       //    thirdNum3 *= -1;
+                                                       //    Console.Write($"{num} - {thirdNum3}");
+                                                       //}
+                                         
+                                         
+    
+    
