7493ea933e15509461ca630b2af738aa277d693b
Maksim <homeice@mail.ru>
2022-09-26 01:56:55 +0300

    Решение задачи

---
+++ Task23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int x = new Random().Next(1, 10);
+Console.Write($"{x} -> ");
+for (int currentX = 1; currentX <= x; currentX++)
+    Console.Write($"{Math.Pow(currentX, 3)}, ");
