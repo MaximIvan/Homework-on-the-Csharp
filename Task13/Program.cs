ebb1ebe3db0c39e4c84c5baa0d7cbf9d5b680a3e
Maksim <homeice@mail.ru>
2022-09-28 20:37:58 +0300

    решение задачи через string

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -8,29 +8,17 @@ int num = new Random().Next();
 
 if ((num < 100) & (num > -100)) Console.Write($"{num} - третьей цифры нет");
 int currentNum = num;
-while ((currentNum >= 1000) | (currentNum <= -1000))   // if (((num < 1000) & (num > 100)) | ((num > -1000) & (num < -100)))
-{                                                      //{
-    currentNum /= 10;                                  //   int thirdNum = num % 10;       
-}                                                      //   if ((thirdNum > -10) & (thirdNum < - 1))
-int thirdNum = currentNum % 10;                        //   thirdNum *= -1;
-if ((thirdNum > -10) & (thirdNum < - 1))               //   Console.Write($"{num} - {thirdNum}");
-thirdNum *= -1;                                        //} 
-Console.Write($"{num} - {thirdNum}");                  //if (((num < 10000) & (num > 1000)) | ((num > -10000) & (num < -1000)))
-                                                       //{
-                                                       //    int numOst = num % 100;
-                                                       //    int thirdNum2 = numOst / 10;
-                                                       //    if ((thirdNum2 > -10) & (thirdNum2 < - 1))
-                                                       //    thirdNum2 *= -1;
-                                                       //    Console.Write($"{num} - {thirdNum2}");
-                                                       //} 
-                                                       //if (((num < 100000) & (num > 10000)) | ((num > -100000) & (num < -10000)))
-                                                       //{
-                                                       //    int numOst2 = num % 1000;
-                                                       //    int thirdNum3 = numOst2 / 100;
-                                                       //    if ((thirdNum3 > -10) & (thirdNum3 < - 1))
-                                                       //    thirdNum3 *= -1;
-                                                       //    Console.Write($"{num} - {thirdNum3}");
-                                                       //}
+while ((currentNum >= 1000) | (currentNum <= -1000))   // решение через string
+{                                                      // string n = Console.ReadLine();
+    currentNum /= 10;                                  // int num = Convert.ToInt32(n);      
+}                                                      // if (num < 100)
+int thirdNum = currentNum % 10;                        // {
+if ((thirdNum > -10) & (thirdNum < - 1))               //   Console.WriteLine("Третьей цифры нет");
+thirdNum *= -1;                                        // } 
+Console.Write($"{num} - {thirdNum}");                  // else if (num < 1000)
+                                                       // {
+                                                       //   Console.WriteLine(n[2]);
+                                                       // }
                                          
                                          
     
