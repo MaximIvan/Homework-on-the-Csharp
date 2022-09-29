e31052522bcf96cbf63ac4ba10b744a8a992e4d7
Maksim <homeice@mail.ru>
2022-09-30 02:09:56 +0300

    Задача 29 решена

---
+++ Task29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] massive = new int [8];
+int i = 0;
+while (i < massive.Length)
+{
+    massive[i] = new Random().Next(1, 100);
+    i++;
+}
+
+int pos = 0;
+int ind = 5;
+while (pos < ind)
+{
+    Console.Write($"{massive[pos]}, ");
+    pos++;
+}
+Console.WriteLine();
+
+while ((pos >= ind) & (pos < massive.Length))
+{
+    Console.Write($"{massive[pos]}, ");
+    pos++;
+}
+
+
