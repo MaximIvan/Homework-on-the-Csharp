210374b75731268abef4c54e819f8083c696c31c
Maksim <homeice@mail.ru>
2022-10-02 00:53:41 +0300

    разбор задачи на семинаре

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -15,3 +15,31 @@ int Num5 = num % 10;
 
 if ((Num1 == Num5) && (Num2 == Num4)) Console.Write($"{num} -> является палиндромом");
 else Console.Write($"{num} -> не является палиндромом");
+
+// чужое решение через массив:
+// int[] array = new int[5];
+// Console.Write("Введите пятизначное число ");
+// int num = Int32.Parse(Console.ReadLine());
+
+// if (num > 1000 || num < 99999) Console.Write("Вы ввели неверное число");
+// for(int i = 0; i < 5; i++)
+// {
+//      array[i] = num % 10;
+//      num = num / 10;
+// }
+// if(array[1] == array[3] & array[0] == array[4]) Console.Write("Введенное число является палиндромом");
+// else Console.Write ("Введенное число не является палиндромом");
+
+// УНИВЕРСАЛЬНЫЙ СПОСОБ (развернуть число):
+// int[] array = new int[5];
+// Console.Write("Введите пятизначное число ");
+// int num = Int32.Parse(Console.ReadLine());
+// int rev = 0;
+// 
+// while (num > 0)
+// {
+//      rev = rev *10 + num % 10;
+//      num = num / 10;
+// }
+// if(rev == num) Console.Write("Введенное число является палиндромом");
+// else Console.Write ("Введенное число не является палиндромом");
